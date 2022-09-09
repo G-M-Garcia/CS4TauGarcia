1d9aff91ddd79b0ac3b7a8dc8bef0d4288930c5d
G-M-Garcia <96185531+G-M-Garcia@users.noreply.github.com>
2022-09-09 11:53:45 +0800

    Update q1ex1.java

--- q1ex1/q1ex1.java
+++ q1ex1/q1ex1.java
@@ -32,7 +32,7 @@ public class Q1Ex1 {
         System.out.println("Years in possession: " + p3age + "\n");
         
         System.out.println("The bag has more years in possession than the sketchpad: " + (p1age > p2age));
-        System.out.println("The sketchpad and the watch are of the same : " + (p2color == p3color));
+        System.out.println("The sketchpad and the watch are of the same color: " + (p2color == p3color));
         System.out.println("The sketchpad has more years in possession than the watch: " + (p2age > p3age));
         
     }
